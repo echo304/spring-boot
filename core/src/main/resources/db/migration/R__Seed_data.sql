bff82c6c735d599f3ba1f1232d7e2344a1f3b78b
Sangboak Lee <echo3042@gmail.com>
2020-10-06 13:06:43 +0900

    Add seeding migration

---
+++ core/src/main/resources/db/migration/R__Seed_data.sql
@@ -0,0 +1,10 @@
+INSERT INTO roles (name, created_date, modified_date)
+VALUES('ADMIN', now(), now()), ('MEMBER', now(), now());
+
+INSERT INTO accounts (name, email, PASSWORD, created_date, modified_date)
+VALUES
+('test1', 'test1@test.com', '$2a$10$IK9hSCKnQfQmGa5gBKfc2.uG3WXzwjtqSKrw1lWpskaLVfKfgwWFO', now(), now()),
+('test2', 'test2@test.com', '$2a$10$IK9hSCKnQfQmGa5gBKfc2.uG3WXzwjtqSKrw1lWpskaLVfKfgwWFO', now(), now());
+
+INSERT INTO account_role_assignments (account_id, role_id)
+VALUES(1, 1), (1, 2), (2, 2);
