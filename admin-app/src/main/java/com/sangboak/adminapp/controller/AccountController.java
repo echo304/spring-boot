14e566c99a6de7baf2bdfb11a0334b52123e5869
Sangboak Lee <echo3042@gmail.com>
2020-10-06 21:44:25 +0900

    Implement account create feature

--- admin-app/src/main/java/com/sangboak/adminapp/controller/AccountController.java
+++ admin-app/src/main/java/com/sangboak/adminapp/controller/AccountController.java
@@ -1,13 +1,19 @@
 package com.sangboak.adminapp.controller;
 
+import com.sangboak.adminapp.dto.AccountCreateRequestDto;
 import com.sangboak.adminapp.dto.AccountListResponseDto;
 import com.sangboak.adminapp.service.AccountService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -22,5 +28,31 @@ public class AccountController {
 
         model.addAttribute("accounts", accounts);
         return "account/index";
+    }
+
+    @GetMapping("/create")
+    public String createPage(@ModelAttribute("accountDto") AccountCreateRequestDto dto) {
+        return "account/create";
+    }
+
+    @PostMapping("/create")
+    public String create(
+            Model model,
+            @Valid AccountCreateRequestDto dto,
+            Errors errors
+    ) {
+        if (errors.hasErrors()) {
+            model.addAttribute("accountDto", dto);
+
+            for (FieldError error: errors.getFieldErrors()) {
+                model.addAttribute(error.getField(), error.getDefaultMessage());
+            }
+
+            return "account/create";
+        }
+
+        accountService.createAccount(dto);
+
+        return "redirect:/accounts";
     }
 }
--- admin-app/src/main/java/com/sangboak/adminapp/service/AccountService.java
+++ admin-app/src/main/java/com/sangboak/adminapp/service/AccountService.java
@@ -1,23 +1,70 @@
 package com.sangboak.adminapp.service;
 
 import com.sangboak.adminapp.config.CustomSecurityUser;
+import com.sangboak.adminapp.dto.AccountCreateRequestDto;
+import com.sangboak.adminapp.dto.AccountListResponseDto;
 import com.sangboak.core.entity.Account;
+import com.sangboak.core.entity.Role;
 import com.sangboak.core.repository.AccountRepository;
+import com.sangboak.core.repository.RoleRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
 @AllArgsConstructor
 public class AccountService implements UserDetailsService {
     final private AccountRepository accountRepository;
+    final private RoleRepository roleRepository;
 
     @Override
     public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
         Account account = accountRepository.findByEmail(userEmail).orElseThrow();
         return new CustomSecurityUser(account);
+    }
+
+    @Transactional
+    public void createAccount(AccountCreateRequestDto dto) {
+
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        dto.setPassword(passwordEncoder.encode(dto.getPassword()));
+
+        Account account = Account.builder()
+                .email(dto.getEmail())
+                .password(dto.getPassword())
+                .name(dto.getName())
+                .build();
+
+        if (dto.isAdmin()) {
+            Role role = roleRepository.findByName("ADMIN").orElseThrow();
+            account.addRole(role);
+        }
+
+        Role role = roleRepository.findByName("MEMBER").orElseThrow();
+        account.addRole(role);
+
+        accountRepository.save(account);
+    }
+
+    @Transactional
+    public List<AccountListResponseDto> getAll() {
+        return accountRepository.findAll()
+                .stream()
+                .map(AccountListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+
+    @Transactional
+    public void deleteAccount(Long id) {
+        accountRepository.deleteById(id);
     }
 }
