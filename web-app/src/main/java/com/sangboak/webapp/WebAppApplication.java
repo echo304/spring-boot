3ddd8342bcc9c984fc39240ef166b51908f8875d
Sangboak Lee <echo3042@gmail.com>
2020-10-06 17:18:24 +0900

    Fix missing modules error

--- web-app/src/main/java/com/sangboak/webapp/WebAppApplication.java
+++ web-app/src/main/java/com/sangboak/webapp/WebAppApplication.java
@@ -2,10 +2,14 @@ package com.sangboak.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @PropertySource(value = "classpath:/data-access.properties")
+@EntityScan("com.sangboak.*")
+@EnableJpaRepositories("com.sangboak.*")
 public class WebAppApplication {
 
 	public static void main(String[] args) {
