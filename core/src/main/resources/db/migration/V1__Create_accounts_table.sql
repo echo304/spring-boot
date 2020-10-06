31ff518ab509f4ef26b2e867da7031949000fa68
Sangboak Lee <echo3042@gmail.com>
2020-10-06 17:11:37 +0900

    Fix V1 migration
    - Set explicitly boolean type to delete field

--- core/src/main/resources/db/migration/V1__Create_accounts_table.sql
+++ core/src/main/resources/db/migration/V1__Create_accounts_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE `accounts` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `created_date` datetime DEFAULT NULL,
   `modified_date` datetime DEFAULT NULL,
-  `deleted` tinyint(1) DEFAULT '0',
+  `deleted` boolean DEFAULT false,
   `email` varchar(50) NOT NULL UNIQUE,
   `name` varchar(50) NOT NULL,
   `password` varchar(200) NOT NULL,
