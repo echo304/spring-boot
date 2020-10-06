2ccc4a0a99f0c6b0d0c2e7b835e7adac08c58ac2
Sangboak Lee <echo3042@gmail.com>
2020-10-06 10:23:33 +0900

    Setting for Core module
    - Enabled JPA auditing
    - 별도 data access 설정 파일 참조

--- core/src/main/java/com/sangboak/core/CoreApplication.java
+++ core/src/main/java/com/sangboak/core/CoreApplication.java
@@ -2,8 +2,12 @@ package com.sangboak.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@PropertySource(value = "classpath:/data-access.properties")
 public class CoreApplication {
 
 	public static void main(String[] args) {
