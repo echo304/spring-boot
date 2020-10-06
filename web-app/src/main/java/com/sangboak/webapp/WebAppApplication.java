93bf6521269704bf4bd711289b7dd73ad4d1ec9d
Sangboak Lee <echo3042@gmail.com>
2020-10-06 21:43:11 +0900

    Enabled JpaAuditing on each client app

--- web-app/src/main/java/com/sangboak/webapp/WebAppApplication.java
+++ web-app/src/main/java/com/sangboak/webapp/WebAppApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaAuditing
 @SpringBootApplication
 @PropertySource(value = "classpath:/data-access.properties")
 @EntityScan("com.sangboak.*")
