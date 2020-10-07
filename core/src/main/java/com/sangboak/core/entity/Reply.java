40ea8c87cfb1923f8d7a885833b2a3635d115bff
Sangboak Lee <echo3042@gmail.com>
2020-10-07 16:46:42 +0900

    Add Reply entity

---
+++ core/src/main/java/com/sangboak/core/entity/Reply.java
@@ -0,0 +1,43 @@
+package com.sangboak.core.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity(name = "replies")
+@SQLDelete(sql = "Update replies SET deleted = 1 where id=?")
+@Where(clause = "deleted = 0")
+public class Reply extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(nullable = false)
+    private Account author;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(nullable = false)
+    private Post post;
+
+    @Column(columnDefinition = "boolean default false")
+    private boolean deleted;
+
+    @Builder
+    public Reply(String content, Post post, Account author) {
+        this.content = content;
+        this.post = post;
+        this.author = author;
+    }
+}
