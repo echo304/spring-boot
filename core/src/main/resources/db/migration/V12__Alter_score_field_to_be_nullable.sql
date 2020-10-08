de800c471250d09cdda503b094501cfc3b268cd4
Sangboak Lee <echo3042@gmail.com>
2020-10-08 16:44:11 +0900

    Migration to support ranking table update

--- core/src/main/resources/db/migration/V12__Alter_score_field_to_be_nullable.sql
+++ core/src/main/resources/db/migration/V12__Alter_score_field_to_be_nullable.sql
@@ -1,1 +1,2 @@
-ALTER TABLE rankings MODIFY score int(11);
+ALTER TABLE rankings MODIFY score int(11) NOT NULL;
+ALTER TABLE rankings MODIFY rank int(11);
---
+++ core/src/main/resources/db/migration/V13__Alter_rank_field_to_be_nullable.sql
@@ -0,0 +1,1 @@
+ALTER TABLE rankings MODIFY rank int(11);
